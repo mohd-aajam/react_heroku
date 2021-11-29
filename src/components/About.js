2747012504ce4f08d893a692577ed3f8c89b90e7
mohd-aajam <aazamtagala@gmail.com>
2021-11-29 20:22:46 +0530

    Deploy dem

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
             <div className="accordion-item">
             <h2 className="accordion-header" id="headingOne">
             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Accordion Item # 1
+          Mohd Aajam # 1
             </button>
         </h2>
         <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
